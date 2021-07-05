bb246ce27c8f5f86536190b989117d4ca03f5bf5
BA1bers <brent.albers@yahoo.com>
2021-07-04 21:39:48 -0700

    worked on markdown questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,61 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require ('util');
+const generateMarkdown = require('./utils/generateMarkdown');
+
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Type the title of your project'
+    },
+    {
+        type: 'input',
+        name: 'purpose',
+        message: 'type in the description of you project'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Installation instructions'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Instructions on how to use yout project'
+    },
+    {
+        type: 'input',
+        name: 'contributors',
+        message: 'contributors for your project'
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'what are your project tests?'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Please select a license',
+        choices: ['']
+
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+
+}
 
 // Function call to initialize app
 init();
